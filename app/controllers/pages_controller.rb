625787be7432a12012387497a9763deefe9b1272
Marie Hess <hess.marietheres@gmail.com>
2020-05-02 13:15:07 +0200

    home style upper part

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :index]
 
   def home
+    @super_categories = SuperCategory.all
+
   end
 
   def index
