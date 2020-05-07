4c0c3041a65f2824d83ed2ff919774d103149a41
Marie Hess <hess.marietheres@gmail.com>
2020-05-07 12:20:58 +0200

    style pages index

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ Category.destroy_all
 SuperCategory.destroy_all
 
 
-wein = SuperCategory.create!(name: 'Wein')
+wein = SuperCategory.create!(name: 'Weine')
 spirtuosen = SuperCategory.create!(name: 'Spirtuosen')
 bubbly = SuperCategory.create!(name: 'Bubbly')
 kaffee = SuperCategory.create!(name: 'Kaffe')
@@ -22,14 +22,14 @@ barthel_spezial = SuperCategory.create!(name: 'Barthel Spezial')
 
 weißwein = Category.create!(name: 'Weißwein', super_category: wein)
 rotwein = Category.create!(name: 'Rotwein', super_category: wein)
-Category.create!(name: 'Rose', super_category: wein)
-Category.create!(name: 'Süßwein', super_category: wein)
+rose = Category.create!(name: 'Rose', super_category: wein)
+süßwein = Category.create!(name: 'Süßwein', super_category: wein)
 
 
-Category.create!(name: 'Wiskey', super_category: spirtuosen)
-Category.create!(name: 'Feinbrand', super_category: spirtuosen)
-Category.create!(name: 'Conac', super_category: spirtuosen)
-Category.create!(name: 'Edelbrand', super_category: spirtuosen)
+wiskey = Category.create!(name: 'Wiskey', super_category: spirtuosen)
+feinbrand = Category.create!(name: 'Feinbrand', super_category: spirtuosen)
+cognac = Category.create!(name: 'Cognac', super_category: spirtuosen)
+edelbrand = Category.create!(name: 'Edelbrand', super_category: spirtuosen)
 
 Category.create!(name: 'Champagner', super_category: bubbly)
 Category.create!(name: 'Sekt', super_category: bubbly)
@@ -44,6 +44,8 @@ Country.create!(name: 'Frankreich')
 header1 = Header.create!(name: 'Aweseome header')
 header2 = Header.create!(name: 'Adalusia ')
 header3 = Header.create!(name: 'Popeldiwutz')
+header4 = Header.create!(name: '
+Bayrischer Feinbrand vom Fritznhof / Chiemgau - Dirnsberg')
 
 Product.create!(name: 'BARTHEL 1 ', description: 'Seleccion especial de Don Tomaso. Ein idealer Begleiter zu jedem Essen.13 vol.%. 6 Monate Barrica', year: 2015, volume: 0.75, price: 5.90, category: rotwein, header:header1)
 Product.create!(name: 'BARTHEL 2 ', description: 'Seleccion especial de Don Tomaso. Ein idealer Begleiter zu jedem Essen.13 vol.%. 6 Monate Barrica', year: 2015, volume: 0.75, price: 5.90, category: rotwein, header:header2)
@@ -51,6 +53,15 @@ Product.create!(name: 'BARTHEL 3 ', description: 'Seleccion especial de Don Toma
 Product.create!(name: 'PAGO DE SALAMANDER  Reserva ', description: 'Vino de Yema
 Kirschrote Farbe. Feine Intensität der Aromen, dezente Holztöne durch 24 monatige Barriquelagerung.Anfängliche feine  Tanine, die nach entspanntem Dekantieren herrliche Aromen freisetzen: Nelke,Tabak,Kaffee. Gute Struktur. Nachhaltig.  100 % Tempranillo.14,5 vol.%', year: 2013, volume: 0.75, price: 15.90, category: rotwein, header: header1)
 Product.create!(name: 'BARTHEL Tempranillo ', description: 'Seleccion especial de Don Tomaso. Ein idealer Begleiter zu jedem Essen.13 vol.%. 6 Monate Barrica', year: 2015, volume: 0.75, price: 5.90, category: rotwein)
+
+
+
+Product.create!(name: 'OBSTBRAND', description: 'Apfel und Birne vom Chiemsee , mild ', year: 2015, volume: 0.75, price: 5.90, category: feinbrand, header: header4)
+Product.create!(name: ' HIMBEERGEIST', description: 'Himbeerbrand vom Chiemsee, feines Aroma, milder Geschmack', year: 2015, volume: 0.75, price: 5.90, category: feinbrand, header: header4)
+Product.create!(name: "WILLIAM'S CHRIST", description: "aus William's Christ Äpfeln vom Chiemsee, typisches Aroma, milder Geschmack", year: 2015, volume: 0.75, price: 5.90, category: feinbrand, header: header4)
+
+
+
 
 
 puts' Seeds erstellt'
