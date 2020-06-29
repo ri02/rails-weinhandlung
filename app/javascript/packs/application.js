b83c0d8d857c545c776e217d891c8d75cbbe4dcc
Marie Hess <hess.marietheres@gmail.com>
2020-06-29 10:43:29 +0200

    card-redesign

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import "bootstrap";
 
 import {toggleSidebar, toggleMenu} from './navbar'
-import {toggleCategorySilder} from './category_silder'
+// import {toggleCategorySilder} from './category_silder'
 import {countryFilter} from './country_filter'
 
 toggleSidebar()
-toggleCategorySilder()
+// toggleCategorySilder()
 toggleMenu()
 countryFilter()
