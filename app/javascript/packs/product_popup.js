4fd04ccf74d85b8b17f872116cdef4e4be7574a0
Marie Hess <hess.marietheres@gmail.com>
2020-07-10 14:17:46 +0200

    counter new items on popup

--- app/javascript/packs/product_popup.js
+++ app/javascript/packs/product_popup.js
@@ -4,6 +4,9 @@ const togglePopup = () => {
   const icon = Array.from(document.querySelectorAll('.shopping-icon'))
   const popup = Array.from(document.querySelectorAll('.popup'))
   const popup_close = Array.from(document.querySelectorAll('.popup-close'))
+  const number = Array.from(document.querySelectorAll('.number'))
+  const plus = Array.from(document.querySelectorAll('.plus'))
+  const minus = Array.from(document.querySelectorAll('.minus'))
 
   const togglePopupItem = (e) => {
 
@@ -17,9 +20,24 @@ const togglePopup = () => {
     popup.forEach( (item) => item.classList.remove('active'))
    }
 
+  const addNumber = (e)  => {
+    const number = e.currentTarget.previousElementSibling
+    const newNumber = parseInt(number.innerHTML, 10) + 1
+    number.innerHTML = newNumber
+  }
+
+  const substractNumber = (e)  => {
+    const number = e.currentTarget.nextElementSibling
+    const newNumber = parseInt(number.innerHTML, 10) - 1
+    number.innerHTML = newNumber
+  }
+
   if (popup){
     popup_close.forEach(item => item.addEventListener('click', removePopup))
     icon.forEach(item => item.addEventListener('click', togglePopupItem))
+    plus.forEach(item => item.addEventListener('click', addNumber))
+    minus.forEach(item => item.addEventListener('click', substractNumber))
+
   }
 
 }
